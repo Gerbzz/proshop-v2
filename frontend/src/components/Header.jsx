2c909fe56587570ce9d4b5afb8504f6a31f860c9
Gerbzz <gerbeneil@gmail.com>
2023-11-02 16:25:33 -0400

    TEST

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+  return <div>Header</div>
+}
+
+export default Header
