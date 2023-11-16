2b5e3e7ccb008a55d1852bbb5a801512444214ce
Gerbzz <gerbeneil@gmail.com>
2023-11-16 18:22:57 -0500

    Starting the Front End/ Serving & Fetching Data

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,39 @@
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../assets/logo.png";
 
 const Header = () => {
-  return <div>Header</div>
-}
+	return (
+		<header>
+			<Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+				<Container>
+					<LinkContainer to='/'>
+						<Navbar.Brand>
+							<img src={logo} alt='ProShop logo' />
+							ProShop
+						</Navbar.Brand>
+					</LinkContainer>
 
-export default Header
+					<Navbar.Toggle aria-controls='basic-navbar-nav' />
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav className='ms-auto'>
+							<LinkContainer to='/cart'>
+								<Nav.Link>
+									<FaShoppingCart /> Cart{" "}
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									<FaUser /> Sign in{" "}
+								</Nav.Link>
+							</LinkContainer>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</header>
+	);
+};
+
+export default Header;
